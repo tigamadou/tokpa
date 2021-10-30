644c9ec44b3c554d6a656c34b93901344a76ea22
Amadou <amadou140589@gmail.com>
2021-10-30 23:51:29 +0100

    subcategoris page added

--- app/controllers/subcategory_controller.rb
+++ app/controllers/subcategory_controller.rb
@@ -1,8 +1,14 @@
 class SubcategoryController < ApplicationController
-  before_action :set_shop_datas
+  before_action :set_shop_datas, :set_subcategory
   def index
   end
 
   def show
+  end
+
+  private
+  def set_subcategory
+    @subcategory = Subcategory.includes([:products,:category]).where("subcategories.slug = '#{params[:subcategory]}'").references(:subcategories, :products).first
+    
   end
 end
