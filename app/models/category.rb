674b314b282ffc528f8ba0e098cb5d292ffcabbe
Amadou <amadou140589@gmail.com>
2021-10-30 06:03:19 +0100

    home categories listes

--- app/models/category.rb
+++ app/models/category.rb
@@ -2,5 +2,6 @@ class Category < ApplicationRecord
     has_one_attached :image
     validates :name, presence: true,uniqueness: :true
     has_many :subcategories,  dependent: :destroy 
+    has_many :products, through: :subcategories
   
 end
