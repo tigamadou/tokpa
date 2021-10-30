175c5fe4b77183811b170cc0ea010cd5ec5164a8
Amadou <amadou140589@gmail.com>
2021-10-30 23:01:17 +0100

    search feature added

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -5,6 +5,10 @@ class HomeController < ApplicationController
 
   def search
     @product = Product.new
+    @results = Product.includes([subcategory:[:category]])
+    @results = @results.where('products.description LIKE :search OR products.name LIKE :search',  search: "%#{params[:q]}%")
+    @results = @results.where(category: {id: params[:category_id]}) if !params[:category_id].nil?
+    
   end
 
   
