5e7e241de3fecd75a421571e5b351c21f546b115
Amadou <amadou140589@gmail.com>
2021-10-31 11:15:55 +0100

    cart product tests done

--- spec/models/address_profile_spec.rb
+++ spec/models/address_profile_spec.rb
@@ -1,10 +1,8 @@
 require 'rails_helper'
 
 RSpec.describe AddressProfile, type: :model do
-  describe 'Association tests' do
-    context 'presence name ' do
+  describe 'association' do
       it { should belong_to(:address)}
-      it { should belong_to(:profile)}      
-    end
+      it { should belong_to(:profile)}
   end
 end
