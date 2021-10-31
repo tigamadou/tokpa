af74f702d6f25c474c7a24e0ab3ed12195589565
Amadou <amadou140589@gmail.com>
2021-10-31 07:57:17 +0100

    models validations edited

--- app/models/option.rb
+++ app/models/option.rb
@@ -1,4 +1,5 @@
 class Option < ApplicationRecord
     has_many :product_options, dependent: :delete_all 
     has_many :products, through: :product_options
+    validates :name, presence: true
 end
--- app/models/product_variant_option.rb
+++ app/models/product_variant_option.rb
@@ -2,5 +2,5 @@ class ProductVariantOption < ApplicationRecord
   belongs_to :product_variant
   belongs_to :product_option
 
-  # validates_uniqueness_of :product_option_id, scope: :product_variant_id
+  
 end
