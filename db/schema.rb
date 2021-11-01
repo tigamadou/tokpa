44af133bb3b08650f827cafb44ad7b8fff8412cf
Amadou <amadou140589@gmail.com>
2021-11-01 18:25:44 +0100

    order feature delivered

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_10_24_232921) do
+ActiveRecord::Schema.define(version: 2021_11_01_172230) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -156,6 +156,7 @@ ActiveRecord::Schema.define(version: 2021_10_24_232921) do
     t.bigint "user_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.boolean "delivered"
     t.index ["user_id"], name: "index_orders_on_user_id"
   end
 
