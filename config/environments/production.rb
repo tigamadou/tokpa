a0636b0342cc2494576f1a2368881373360a1de4
Amadou <amadou140589@gmail.com>
2021-11-02 08:08:34 +0100

    sendgrid installed

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -117,15 +117,21 @@ Rails.application.configure do
   # config.active_record.database_selector = { delay: 2.seconds }
   # config.active_record.database_resolver = ActiveRecord::Middleware::DatabaseSelector::Resolver
   # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
-  ActionMailer::Base.smtp_settings = {
-    :port           => ENV['MAILGUN_SMTP_PORT'],
-    :address        => ENV['MAILGUN_SMTP_SERVER'],
-    :user_name      => ENV['MAILGUN_SMTP_LOGIN'],
-    :password       => ENV['MAILGUN_SMTP_PASSWORD'],
-    :domain         => 'spooky-village-69817.herokuapp.com',
-    :authentication => :plain,
-  }
-  ActionMailer::Base.delivery_method = :smtp
   config.action_mailer.default_url_options = { :host => 'spooky-village-69817.herokuapp.com' }
-
+  ActionMailer::Base.delivery_method = :smtp
+  ActionMailer::Base.smtp_settings = {
+    # :port           => ENV['SMTP_PORT'],
+    # :address        => ENV['SMTP_SERVER'],
+    # :user_name      => ENV['SMTP_LOGIN'],
+    # :password       => ENV['SMTP_PASSWORD'],
+    # :domain         => 'spooky-village-69817.herokuapp.com',
+    # :authentication => :plain,
+    user_name: ENV['SENDGRID_USERNAME'],
+    password: ENV['SENDGRID_PASSWORD'],
+    domain: "spooky-village-69817.herokuapp.com",
+    address: "smtp.sendgrid.net",
+    port: 587,
+    authentication: :plain,
+    enable_starttls_auto: true
+  }
 end
